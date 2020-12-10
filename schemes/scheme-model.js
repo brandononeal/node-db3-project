75e4e9e93d5a64b9d8d6b51da8ff9c145576d919
brandononeal <baoneal95@gmail.com>
2020-12-09 19:05:22 -0800

    mvp complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,35 @@
-// scheme-model
+const db = require("../data/db-config");
+
+module.exports = {
+  find() {
+    return db("schemes");
+  },
+  findById(id) {
+    return db("schemes").where("id", id).first();
+  },
+  findSteps(id) {
+    return db("steps as stp")
+      .join("schemes as sch", "stp.scheme_id", "sch.id")
+      .select(
+        "stp.id",
+        "sch.scheme_name",
+        "stp.step_number",
+        "stp.instructions"
+      )
+      .where("sch.id", id)
+      .orderBy("stp.step_number");
+  },
+  add(scheme) {
+    return db("schemes")
+      .insert(scheme)
+      .then(([id]) => {
+        return db("schemes").where("id", id).first();
+      });
+  },
+  update(changes, id) {
+    return db("schemes").where("id", id).update(changes);
+  },
+  remove(id) {
+    return db("schemes").where("id", id).del();
+  },
+};
